f93dd27dc59e864b5f866b8aa8cde7c9e0113fe6
Alex Ryan <alexryan@asana.com>
2015-08-11 10:59:29 -0700

    Added icon to build process

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# First, install our app's dependencies
 cd app/
 npm install
 cd ..
 
+# Install the electron-packager tool
 npm i electron-packager -g
 
-electron-packager app/ threedots --platform=darwin --arch=all --version=0.30.4 --overwrite --out=releases --app-version=2.0.0 --app-bundle-id=com.threedotsapp --helper-bundle-id=com.threedotsapp.helper --asar
+# Use the electron-packager tool. Modify these arguments accordingly.
+electron-packager app/ threedots --platform=darwin --arch=all --version=0.30.4 --asar --overwrite --out=releases --app-version=2.0.0 --app-bundle-id=com.threedotsapp --helper-bundle-id=com.threedotsapp.helper --icon=icon.icns
+
