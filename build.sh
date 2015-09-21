07465680fe5c63d5cc032df5639da80057e1257f
Alex Ryan <alex@alexryan.org>
2015-09-21 11:18:49 -0700

    Upgrade to new release of Electron

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ cd ..
 npm i electron-packager -g
 
 # Use the electron-packager tool. Modify these arguments accordingly.
-electron-packager app/ threedots --platform=darwin --arch=all --version=0.30.4 --asar --overwrite --out=releases --app-version=2.0.0 --app-bundle-id=com.threedotsapp --helper-bundle-id=com.threedotsapp.helper --icon=icon.icns
+electron-packager app/ threedots --platform=darwin --arch=all --version=0.33.0 --asar --overwrite --out=releases --app-version=2.0.0 --app-bundle-id=com.threedotsapp --helper-bundle-id=com.threedotsapp.helper --icon=icon.icns
 
