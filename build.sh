550b934871bd7c7c09a923eed4df4d91a0176465
Alex Ryan <alex@alexryan.org>
2015-09-21 11:36:32 -0700

    Fix build script to only attempt to install electron-packager if needed

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ npm install
 cd ..
 
 # Install the electron-packager tool
-npm i electron-packager -g
+which electron-packager || npm i electron-packager -g
 
 # Use the electron-packager tool. Modify these arguments accordingly.
 electron-packager app/ threedots --platform=darwin --arch=all --version=0.33.0 --asar --overwrite --out=releases --app-version=2.0.0 --app-bundle-id=com.threedotsapp --helper-bundle-id=com.threedotsapp.helper --icon=icon.icns
